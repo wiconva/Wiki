f2a536ee9e3e540e97a63164743d393ce6f977c2
wiconva <wiconva@gmail.com>
2023-07-09 13:49:19 -0500

    Working with content and java script features.

---
+++ src/main/web/javascript/mijavascript.js
@@ -0,0 +1,29 @@
+//Regresa a la barra de contenido cuando se da click en los títulos.
+function showHeaderView (e) {
+   var evento = e || window.event;
+   var id = evento.target.id;
+   var elementos = document.getElementsByTagName ("a")
+   var href = "";
+   var targetElement = "";
+   for (var i=0; i<elementos.length; i++){
+        href = elementos[i].getAttribute("href");
+        if (href.includes(("#"+id)) && id != ""){
+            targetElement= elementos[i];
+        }
+   }
+
+   if(targetElement == ""){
+        document.getElementById("header").scrollIntoView();
+   }else {
+        targetElement.scrollIntoView();
+   }
+}
+
+// Ajusta el tamaño de de la barra de contenido en función del contenido de la section.
+function fixAsideHeight(){
+    var section = document.getElementById("section");
+    var aside = document.getElementById("aside");
+    aside.style.height = section.clientHeight+"px"  ;
+
+
+}
