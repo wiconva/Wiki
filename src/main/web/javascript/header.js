abbd6b65b2b16f44833952e14a3075753cb758bf
wiconva <wiconva@gmail.com>
2023-07-22 12:53:45 -0500

    Working on cmd content.

--- src/main/web/javascript/header.js
+++ src/main/web/javascript/header.js
@@ -3,4 +3,6 @@ document.write("<nav><a href=\"../../index.html\">Inicio</a>"+
     "<a href=\"../html/html.html\">HTML</a>"+
     "<a href=\"../css/css.html\">CSS</a>"+
     "<a href=\"../javascript/javascript.html\">Java Script</a>"+
+    "<a href=\"../cmd/cmd.html\">CMD</a>"+
+    "<a href=\"../maven/maven.html\">Maven</a>"+
     "</nav>");
