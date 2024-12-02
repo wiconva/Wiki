c4cd06756855e86c8aa295d558d7fa17e067d669
wiconva <wiconva@gmail.com>
2024-12-01 19:11:50 -0500

    Adding JPA module.

--- src/main/web/javascript/header.js
+++ src/main/web/javascript/header.js
@@ -1,9 +1,11 @@
 //Agrega el encabezado en cada página.
-document.write("<nav><a href=\"../../index.html\">Inicio</a>"+
-    "<a href=\"../html/html.html\">HTML</a>"+
-    "<a href=\"../css/css.html\">CSS</a>"+
-    "<a href=\"../javascript/javascript.html\">Java Script</a>"+
-    "<a href=\"../cmd/cmd.html\">CMD</a>"+
-    "<a href=\"../maven/maven.html\">Maven</a>"+
-    "<a href=\"../testing/testing.html\">Testing</a>"+
-    "</nav>");
+document.write("<nav>"
+    +"<a href=\"../java/java.html\">Java</a>"
+    +"<a href=\"../html/html.html\">HTML</a>"
+    +"<a href=\"../css/css.html\">CSS</a>"
+    +"<a href=\"../javascript/javascript.html\">Java Script</a>"
+    +"<a href=\"../cmd/cmd.html\">CMD</a>"
+    +"<a href=\"../maven/maven.html\">Maven</a>"
+    +"<a href=\"../testing/testing.html\">Testing</a>"
+    +"<a href=\"../testing/testing.html\">Testing</a>"
+    +"</nav>");
--- src/main/web/javascript/mijavascript.js
+++ src/main/web/javascript/mijavascript.js
@@ -1,22 +1,13 @@
 //Regresa a la barra de contenido cuando se da click en los títulos.
 function showHeaderView (e) {
-   var evento = e || window.event;
-   var id = evento.target.id;
    var elementos = document.getElementsByTagName ("a")
    var href = "";
    var targetElement = "";
-   for (var i=0; i<elementos.length; i++){
-        href = elementos[i].getAttribute("href");
-        if (href.includes(("#"+id)) && id != ""){
-            targetElement= elementos[i];
-        }
-   }
-
-   if(targetElement == ""){
-        document.getElementById("header").scrollIntoView();
-   }else {
-        targetElement.scrollIntoView();
-   }
+   console.log(e.srcElement.type)
+   if (!(e.srcElement.type=="textarea")){
+       console.log("entro al area")
+       document.getElementById("header").scrollIntoView();
+    }
 }
 
 // Ajusta el tamaño de de la barra de contenido 'aside' en función del contenido de la 'section'.
